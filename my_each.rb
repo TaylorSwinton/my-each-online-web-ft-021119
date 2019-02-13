25b5f4d448646eb5fe86f9c688238d02b2702458
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-13 18:05:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,8 @@
 def my_each(words)
-  if blockgiven?
     i = 0
     while i < words.length
       yield words[i]
       i = i + 1
     end
     words
-  else
-    "Hey! No block was given"
-  end
 end
